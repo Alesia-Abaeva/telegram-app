cd7fb6352dca5d6d5b5fb1bfd18037fc1b63d6d0
Alesia <alesia.litvinenko@gmail.com>
2025-02-25 14:30:45 +0300

    Update for build

--- src/layout/Navigation/Navigation.tsx
+++ src/layout/Navigation/Navigation.tsx
@@ -1,19 +1,27 @@
 import React from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-import { Icon, IconNames } from "../../component/Icon";
+import { Icon } from "../../component/Icon";
 import { navigation } from "./config";
 
 function Navigation() {
   const navigate = useNavigate();
   const location = useLocation();
 
-  const [currentScreen, setCurrentScreen] = React.useState("/");
+  console.log(location);
+
+  const [currentScreen, setCurrentScreen] = React.useState(location.pathname);
+
+  const navigateHandler = (path: string) => {
+    navigate(path);
+    setCurrentScreen(path);
+  };
 
   return (
     <nav className="fixed px-[6px] text-white bottom-2 left-4 right-4 rounded-lg bg-[#17288E] flex justify-around items-center h-[76px] z-50">
       {navigation.map(({ href, iconName, name }) => (
         <div
           key={name}
+          onClick={() => navigateHandler(href)}
           className={`flex flex-col items-center justify-center w-14 h-14 rounded-lg ${
             currentScreen === href ? "bg-[#464E8A]" : "bg-[#17288E] "
           }`}
